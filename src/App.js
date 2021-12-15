04b374e199e9ef7dd1e80c50b8fe9a1e761944ad
BunniMK <86545296+BunniMK@users.noreply.github.com>
2021-12-14 17:37:03 -0700

    Working on logout feature

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom';
 
 import Login from './components/Login';
 import FriendsList from './components/FriendsList';
 import AddFriend from './components/AddFriend';
+import Logout from './components/Logout'
 
 function App() {
   return (
-    <div className="App">
-      <header>
+    <Router className="App">
+      <header> 
+        <Link className='link' to='/'>Login</Link>
         <Link className='link' to='/friends/add'>Add a friend</Link>
         <Link className='link' to='/friends'> Friends</Link>
-        <Link className='link' to='/'>Login</Link>
+        <Link className='link' to='/logout'>Logout</Link>
       </header>
 
       <Switch>
-        <Route path='/logout' />
+        <Route path='/logout' component={Logout}/>
         <Route path='/friends/add' component={AddFriend}/>
         <Route path='/friends' component={FriendsList}/>
         <Route path='/login' component={Login}/>
         <Route path='/' component={Login}/>
       </Switch>
 
-    </div>
+    </Router>
   );
 }
 
